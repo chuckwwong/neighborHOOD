5f5d591163350b5fba9cf834da9f6c8c7a9900f4
Jabbarzadeh <jabbarz2@sp19-cs411-37.cs.illinois.edu>
2019-04-08 22:59:52 -0500

    changed 'customer' to 'crime'

--- neighborhood/users/views.py
+++ neighborhood/users/views.py
@@ -27,29 +27,29 @@ def user_list(request):
  
  
 @csrf_exempt 
-def customer_detail(request, pk):
+def crime_detail(request, pk):
     try: 
-        customer = Customer.objects.get(pk=pk) 
-    except Customer.DoesNotExist: 
+        crime = Crime.objects.get(pk=pk) 
+    except Crime.DoesNotExist: 
         return HttpResponse(status=status.HTTP_404_NOT_FOUND) 
  
     if request.method == 'GET': 
-        customer_serializer = CustomerSerializer(customer) 
-        return JsonResponse(customer_serializer.data) 
+        crime_serializer = CrimeSerializer(crime) 
+        return JsonResponse(crime_serializer.data) 
  
     elif request.method == 'PUT': 
-        customer_data = JSONParser().parse(request) 
-        customer_serializer = CustomerSerializer(customer, data=customer_data) 
-        if customer_serializer.is_valid(): 
-            customer_serializer.save() 
-            return JsonResponse(customer_serializer.data) 
-        return JsonResponse(customer_serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+        crime_data = JSONParser().parse(request) 
+        crime_serializer = CrimeSerializer(crime, data=crime_data) 
+        if crime_serializer.is_valid(): 
+            crime_serializer.save() 
+            return JsonResponse(crime_serializer.data) 
+        return JsonResponse(crime_serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
  
     elif request.method == 'DELETE': 
-        customer.delete() 
+        crime.delete() 
         return HttpResponse(status=status.HTTP_204_NO_CONTENT) 
 @csrf_exempt
-def customer_list_location(request, location):
+def crime_list_location(request, location):
     crimes = Crime.objects.filter(location=location)
         
     if request.method == 'GET': 
